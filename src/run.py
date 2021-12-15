f34a152ce977459001c9e67bd8058be5233f64a1
Onur Ovalı <onurovali@Onurs-SPI-MacBook-Pro.local>
2021-12-15 03:49:19 +0300

    aa

--- src/run.py
+++ src/run.py
@@ -1,19 +1,19 @@
-from src import excel
-from src import request_operations as ro
+#from src import excel
+#from src import request_operations as ro
 
 print("hola mundo!")
 
 #ro.time_to_first_byte()
 
-kariyer_jobads = excel.read_kariyer_jobads()
-f3 = kariyer_jobads[:3]
+#kariyer_jobads = excel.read_kariyer_jobads()
+#f3 = kariyer_jobads[:3]
 #print(f3)
 
-for i in range(1):
-    jobad_id = f3['Ilan Kodu'][i]
-    jobad_text = f3['AllText'][i]
-    #print("Job ad ID: " + str(jobad_id) + ", Job ad text: " + jobad_text)
-    ro.get_jobad_risk_score('kariyer', jobad_id, jobad_text)
+#for i in range(1):
+#    jobad_id = f3['Ilan Kodu'][i]
+#    jobad_text = f3['AllText'][i]
+#    #print("Job ad ID: " + str(jobad_id) + ", Job ad text: " + jobad_text)
+#    ro.get_jobad_risk_score('kariyer', jobad_id, jobad_text)
 
 #for job_ad in first_3_kariyer_jobads:
 #    print(job_ad)
