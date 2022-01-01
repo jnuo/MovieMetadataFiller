e2f6d8fef015735bdc77ac1b94a654310ed373d2
Onur Ovalı <onurovali@Onurs-SPI-MacBook-Pro.local>
2022-01-02 00:49:38 +0300

    aaa

--- src/run.py
+++ src/run.py
@@ -1,20 +1,28 @@
 import excel
 import request_operations as ro
+import scrapeImdb as im
 
 # Create Virtual Environment        >python3 -m venv .venv
 # Activate Virtual Environment      >source .venv/bin/activate
 # Install a package into the Env    >python3 -m pip install openpyxl
 # De-activate Virtual Environment   >deactivate
 
-msg = "hola mundo"
-print(msg)
+# git init
+# git status
+# git add 'filename'
+# git commit 'filename'
+# git push
 
-kariyer_jobads = excel.read_kariyer_jobads()
-f3 = kariyer_jobads[:3]
-print(f3)
+#msg = "hola mundo"
+#print(msg)
+
+#kariyer_jobads = excel.read_kariyer_jobads()
+#f3 = kariyer_jobads[:3]
+#print(f3)
+
+#ro.time_to_first_byte()
 
 
-
-ro.time_to_first_byte()
-
-
+tenet_url = "https://www.imdb.com/title/tt6723592/"
+m = im.getMovieScore(tenet_url)
+print(m)
